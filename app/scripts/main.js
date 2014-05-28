a7d9ce479011b543e953ef64eb3e4f17b083205a
LudoBermejo <LudoBermejo@gmail.com>
2014-05-28 16:20:10 +0200

    Free

---
+++ app/scripts/main.js
@@ -0,0 +1,23 @@
+
+require.config({
+    baseUrl: 'scripts',
+    paths: {
+        "components": "../bower_components",
+        "modernizr": "../bower_components/modernizr/modernizr",
+        'easeljs': '../../bower_components/easeljs/lib/easeljs-0.7.1.min',
+        'preloadjs': '../../bower_components/PreloadJS/lib/preloadjs-NEXT.min',
+        "collisionDetection": "components/ndgmr.Collision"
+    },
+    shim: {
+        'preloadjs': {
+            //These script dependencies should be loaded before loading
+            //backbone.js
+            deps: ['easeljs']
+        }
+    }
+});
+
+require(["world/GameWorld"], function(gameWorld)
+{
+    gameWorld.init();
+});
---
+++ app/scripts/world/GameWorld.js
@@ -0,0 +1,58 @@
+define(["preloadjs", "world/ludo/Ludo", "world/map/Map"], function (preload, Ludo, Map) {
+        //return an object to define the "my/shirt" module.
+
+        var GameWorld = {};
+
+        var manifest;
+        var loader;
+        var stage;
+
+
+        var handleComplete = function () {
+
+
+            window.createjs.Ticker.timingMode = window.createjs.Ticker.RAF_SYNCHED;
+            window.createjs.Ticker.addEventListener("tick", tick);
+
+            window.createjs.Ticker.framerate = 60;
+
+            Map.init(stage, loader);
+            Ludo.init(stage, loader, Map.getCollision);
+
+            stage.update();
+
+        };
+
+        function tick() {
+            Ludo.movement();
+        }
+
+        GameWorld.init = function () {
+
+
+            stage = new window.createjs.Stage("worldCanvas");
+
+
+            stage.customUpdate = function()
+            {
+                stage.update();
+            };
+
+
+            manifest = [
+                {src: "assets/LudoSprite.png", id: "Ludo"},
+                {src: "assets/map/MapaLudo.json", id: "MapJSON"},
+                {src: "assets/map/MapaLudo.png", id: "MapImage"}
+
+            ];
+
+            loader = new window.createjs.LoadQueue(false);
+            loader.addEventListener("complete", handleComplete);
+            loader.loadManifest(manifest);
+
+        };
+
+
+        return GameWorld;
+    }
+);
