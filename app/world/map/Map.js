69b730ef72967533bcb0756a31f54ec91f43d82f
LudoBermejo <LudoBermejo@gmail.com>
2014-05-28 10:43:27 +0200

    Collision detection done

--- app/world/map/Map.js
+++ app/world/map/Map.js
@@ -1,4 +1,4 @@
-define(["preloadjs"], function () {
+define(["preloadjs", "collisionDetection"], function () {
         //return an object to define the "my/shirt" module.
 
         var Map = {};
@@ -12,30 +12,27 @@ define(["preloadjs"], function () {
 
         var background;
         var arrayCollision = [];
-        var border;
 
+        var customSprite;
 
 
         Map.init = function (stage, load) {
 
-            border = new createjs.Shape();
             background = new createjs.Container();
             // layer initialization
             function initLayer(layerData, tilesetSheet, tilewidth, tileheight) {
 
 
-
                 var layer = new createjs.Container;
 
-                layer.name =layerData.name;
+                layer.name = layerData.name;
 
-                if(layerData.name.indexOf("Collision") >-1)
-                {
+                if (layerData.name.indexOf("Collision") > -1) {
 
                 }
-                for ( var y = 0; y < layerData.height; y++) {
+                for (var y = 0; y < layerData.height; y++) {
 
-                    for ( var x = 0; x < layerData.width; x++) {
+                    for (var x = 0; x < layerData.width; x++) {
 
                         // create a new Bitmap for each cell
                         var cellBitmap = new createjs.Sprite(tilesetSheet);
@@ -43,7 +40,7 @@ define(["preloadjs"], function () {
                         var idx = x + y * layerData.width;
                         // tilemap data uses 1 as first value, EaselJS uses 0 (sub 1 to load correct tile)
 
-                        if(layerData.data[idx] - 1 > -1) {
+                        if (layerData.data[idx] - 1 > -1) {
                             cellBitmap.gotoAndStop(layerData.data[idx] - 1);
                             // isometrix tile positioning based on X Y order from Tiled
                             cellBitmap.x = x * tilewidth - (x);
@@ -54,20 +51,17 @@ define(["preloadjs"], function () {
                             // add bitmap to stage
                             layer.addChildAt(cellBitmap, 0);
 
-                            if(layerData.name.indexOf("Collision") >-1)
-                            {
+                            if (layerData.name.indexOf("Collision") > -1) {
                                 //var text = new createjs.Text(x + ":" + y, "10px Arial", "#ff7700"); text.x =  x * tilewidth; text.y = y* tileheight;
                                 //layer.addChildAt(text, 0);
 
                                 arrayCollision[x + ":" + y] = cellBitmap;
+                                cellBitmap.name = layerData.name + ":" + x + ":" + y;
 
 
                             }
 
                         }
-
-
-
 
 
                     }
@@ -79,81 +73,75 @@ define(["preloadjs"], function () {
 
             }
 
-            var checkIntersection = function(rect1,rect2) {
-                if ( rect1.x >= rect2.x + rect2.width || rect1.x + rect1.width <= rect2.x || rect1.y >= rect2.y + rect2.height || rect1.y + rect1.height <= rect2.y ) return false;
+            var checkIntersection = function (rect1, rect2) {
+                if (rect1.x >= rect2.x + rect2.width || rect1.x + rect1.width <= rect2.x || rect1.y >= rect2.y + rect2.height || rect1.y + rect1.height <= rect2.y) return false;
                 return true;
             }
 
-            this.getCollision = function(obj, x, y)
-            {
+            this.getCollision = function (obj, x, y) {
+
+                if(customSprite == undefined)
+                {
+                    customSprite = obj.clone();
+
+                    customSprite.scaleX = customSprite.scaleY = 0.5;
+                    customSprite.visible = false;
+
+
+                    stage.addChild(customSprite);
+
+                }
+
+
+
 
                 var rect = obj.getBounds();
 
-                console.log(rect);
-                rect.x = Math.floor(obj.x/(wTile));
-                rect.y = Math.floor(obj.y/(hTile));
-                rect.width = Math.ceil((rect.width)/(wTile))+1;
-                rect.height = Math.ceil((rect.height)/(hTile))+1;
+                rect.x = Math.floor((obj.x + x) / (wTile - 1));
+                rect.y = Math.floor((obj.y + y) / (hTile - 1));
+                rect.width = Math.ceil((rect.width + x) / (wTile - 1));
+                rect.height = Math.ceil((rect.height + y) / (hTile - 1));
 
-                border.graphics.clear();
-                border.graphics.beginStroke("#000");
-                border.graphics.setStrokeStyle(1);
-                border.snapToPixel = true;
-                border.graphics.drawRect(0, 0, rect.width*wTile, rect.height*wTile);
-                border.x = rect.x*wTile;
-                border.y = rect.y*hTile;
-
-                stage.addChild(border);
 
 
                 var arrayCheck = [];
 
-                console.log("---")
-                console.log(rect.x)
-                console.log(rect.width)
-                for(var i= rect.x;i<=rect.x+rect.width;i++)
-                {
-                    for(var j=rect.y;j<=rect.y+rect.height;j++)
-                    {
-                        console.log("Busco " + i + ":" + j)
+                for (var i = rect.x; i <= rect.x + rect.width; i++) {
+                    for (var j = rect.y; j <= rect.y + rect.height; j++) {
 
-                        if(arrayCollision[i+":"+j] != undefined)
-                        {
-                            arrayCheck.push(arrayCollision[i+":"+j])
+                        if (arrayCollision[i + ":" + j] != undefined) {
+                            arrayCheck.push(arrayCollision[i + ":" + j])
                         }
                     }
                 }
 
 
-                if(arrayCheck.length)
-                {
-                    console.log("GOLPEO");
-                    var rect1 = { x: obj.x+obj.width/2+x, y: obj.y+obj.height/2+y, width: 5, height: 5}
+                if (arrayCheck.length) {
+
+                    customSprite.x = obj.x + (obj.width/2 - obj.width*0.5/2);
+                    customSprite.y = obj.y+ (obj.height/2 - obj.height*0.5/2);
+
+                    customSprite.x += x;
+                    customSprite.y += y;
 
 
 
 
-                    console.log(rect1);
-                    for(var i=0;i<= arrayCheck.length-1;i++){
-                            var sprite =  arrayCheck[i];
-                            var rect2 = { x: sprite.x, y: sprite.y, width: wTile, height: hTile}
+                    for (var i = 0; i <= arrayCheck.length - 1; i++) {
 
-                            sprite.visible = false;
+                        var collision = ndgmr.checkPixelCollision(arrayCheck[i], customSprite, 0, true);
 
 
-                            if(checkIntersection(rect1, rect2))
-                            {
-                                console.log("SI");
-                                return true;
-                            }
+
+                        if (collision) {
+
+                            return true;
+                        }
+
                     }
 
+
                 }
-
-
-
-
-
 
 
                 return false;
@@ -172,10 +160,10 @@ define(["preloadjs"], function () {
 
             console.log(wTile);
             var imageData = {
-                images : [ load.getResult("MapImage") ],
-                frames : {
-                    width : wTile,
-                    height : hTile
+                images: [ load.getResult("MapImage") ],
+                frames: {
+                    width: wTile,
+                    height: hTile
                 }
             };
             // create spritesheet
@@ -191,10 +179,9 @@ define(["preloadjs"], function () {
 
             st.addChild(background)
 
-            //background.cache(0,0, mapData.tilewidth*mapData.width, mapData.tileheight*mapData.height)
+            background.cache(0, 0, mapData.tilewidth * mapData.width, mapData.tileheight * mapData.height)
 
         }
-
 
 
         return Map;
